e4c1f4c34f827f7c950b9a531cd617d2d8d72cc2
suvarnaTT <39673923+suvarnaTT@users.noreply.github.com>
2018-05-27 08:01:41 -0700

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,15 @@
+class program1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("program start");
+		int n1;
+		int n2;
+		int sum;
+		n1=30;
+		n2=60;
+		sum=n1+n2;
+		System.out.println(sum);
+		System.out.println("program ends");
+	}
+}
